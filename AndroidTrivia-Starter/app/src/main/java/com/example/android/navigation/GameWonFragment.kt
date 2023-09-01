db06fe8ba5cebdf7ac9d468a637d5deb586fdde1
jie66 <jie669928@gmail.com>
2023-09-01 12:47:46 +0800

    Data binding(live data)

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -33,7 +33,7 @@ class GameWonFragment : Fragment() {
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
 
-
+        // Add OnClick Handler for Next Match button
         binding.nextMatchButton.setOnClickListener { view:View ->
             view.findNavController()
                 .navigate(R.id.action_gameWonFragment_to_gameFragment)
